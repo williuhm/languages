788bab104568ccfab673ea4098ee46fadf9e9373
William Lister <billy09lister@gmail.com>
2026-04-08 15:18:48 -0500

    Finished W3Schools Data Types Tutorial

---
+++ C++/016-Characters.cpp
@@ -0,0 +1,17 @@
+/*
+    Characters are pretty universal as well, not worried about this.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char myGrade = 'B';
+    cout << myGrade << "\n";
+
+    char a = 65, b = 66, c = 67;
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    return 0;
+}
