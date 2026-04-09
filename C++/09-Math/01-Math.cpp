6b7704a69942cb4e73808b0e9744c0a9bcc6954b
William Lister <william.lister@nasa.gov>
2026-04-09 09:22:50 -0500

    Finished W3Schools Math Tutorial.

---
+++ C++/09-Math/01-Math.cpp
@@ -0,0 +1,18 @@
+/*
+    Small section about max and min functions, maybe some other stuff.
+*/
+
+#include <iostream>
+// Include the cmath library
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << max(5, 10) << "\n";
+    cout << min(5, 10) << "\n";
+    
+    cout << sqrt(64) << "\n";
+    cout << round(2.6) << "\n";
+    cout << log(2) << "\n";
+    return 0;
+}
