60e9dd5219f71e99a41cec34610604bd62bc5336
William Lister <william.lister@nasa.gov>
2026-04-09 08:08:10 -0500

    Finished W3Schools Comments Tutorial.

---
+++ C++/03-Comments/01-Comments.cpp
@@ -0,0 +1,15 @@
+/*
+    I've already been writing comments, so this won't be too useful.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This is a comment
+    cout << "Hello World!"; // This is a comment
+    /* The code above will print the words Hello World!
+    to the screen, and it is amazing */
+}
+
+// No challenge here.
