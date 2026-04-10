9ea9477c5c72312eedfd21f845470a36677bedd9
William Lister <billy09lister@gmail.com>
2026-04-10 11:58:58 -0500

    Finished W3Schools References Tutorial.

---
+++ C++/19-References/02-Memory-Address.cpp
@@ -0,0 +1,17 @@
+/*
+    Ok I assume this is the difficult part.
+
+    Nope, this is too short as well... Guess
+    it might be a C thing.
+
+    Never mind, it was pointers, up next...
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string food = "Pizza";
+    cout << &food << "\n"; // Outputs 0x6dfed4
+    return 0;
+}
