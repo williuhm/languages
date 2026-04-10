7a129c7b0aafd3c5b17fe60cb8ebc1a9554327b5
William Lister <william.lister@nasa.gov>
2026-04-10 15:17:08 -0500

    Finished W3Schools Encapsulation Tutorial.

---
+++ C++/32-Encapsulation/01-Encapsulation.cpp
@@ -0,0 +1,30 @@
+/*
+    One of the four major principles of OOP. This
+    talks about the getter and setter methods.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Employee {
+    private:
+        // Private attribute
+        int salary;
+
+    public:
+        // Setter
+        void setSalary(int s) {
+            salary = s;
+        }
+        // Getter
+        int getSalary() {
+            return salary;
+        }
+};
+
+int main() {
+    Employee myObj;
+    myObj.setSalary(50000);
+    cout << myObj.getSalary() << "\n";
+    return 0;
+}
