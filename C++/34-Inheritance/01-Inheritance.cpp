df3a3edbcf56430d9f7737cd54f5d6c3bd63b171
William Lister <billy09lister@gmail.com>
2026-04-13 08:25:33 -0500

    Finished W3Schools Inheritance Tutorial.

---
+++ C++/34-Inheritance/01-Inheritance.cpp
@@ -0,0 +1,29 @@
+/*
+    This is another major principle of OOP. I think it
+    has to do with parents and children classes.
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+    public:
+        string brand = "Ford";
+        void honk() {
+            cout << "Tuut, tuut! \n" ;
+        }
+};
+
+// Derived class
+class Car: public Vehicle {
+    public:
+        string model = "Mustang";
+};
+
+int main() {
+    Car myCar;
+    myCar.honk();
+    cout << myCar.brand + " " + myCar.model << "\n";
+    return 0;
+}
---
+++ C++/34-Inheritance/02-Multilevel-Inheritance.cpp
@@ -0,0 +1,29 @@
+/*
+    Sounds like child of a child of a parent. I think priority
+    goes to the lowest level, then upwards.
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base class (parent)
+class MyClass {
+    public:
+        void myFunction() {
+            cout << "Some content in parent class." << "\n" ;
+        }
+};
+
+// Derived class (child)
+class MyChild: public MyClass {
+};
+
+// Derived class (grandchild)
+class MyGrandChild: public MyChild {
+};
+
+int main() {
+    MyGrandChild myObj;
+    myObj.myFunction();
+    return 0;
+}
