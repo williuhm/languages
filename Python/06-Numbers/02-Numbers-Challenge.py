62ce3d6849d8814f2f09c0304e2f21a7e167419e
William Lister <billy09lister@gmail.com>
2026-04-16 10:29:58 -0500

    Finished W3Schools Numbers Tutorial.

---
+++ Python/06-Numbers/02-Numbers-Challenge.py
@@ -0,0 +1,25 @@
+"""
+    Challenge: Numbers
+        Test your understanding of Python number types by completing a small coding challenge.
+
+    Instructions
+        Inside the editor, complete the following steps:
+        1. Create a variable x and assign it the integer 5
+        2. Create a variable y and assign it the float 3.14
+        3. Create a variable z and assign it the complex number 2+3j
+        4. Print the type of each variable using type()
+"""
+
+# Create an integer
+x = 5
+
+# Create a float
+y = 3.14
+
+# Create a complex number
+z = 2+3j
+
+# Print the types
+print(type(x))
+print(type(y))
+print(type(z))
