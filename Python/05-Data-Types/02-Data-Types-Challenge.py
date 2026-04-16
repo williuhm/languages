8fea647a257f4c169139af0dbad2ae69c90aed3b
William Lister <billy09lister@gmail.com>
2026-04-16 10:24:26 -0500

    Finished W3Schools Data Types Tutorial.

---
+++ Python/05-Data-Types/02-Data-Types-Challenge.py
@@ -0,0 +1,21 @@
+"""
+    Challenge: Data Types
+        Test your understanding of Python data types by completing a small coding challenge.
+
+    Instructions
+        Inside the editor, complete the following steps:
+        1. Create a variable x with the value 5
+        2. Create a variable y with the value 3.14
+        3. Create a variable z with the value "Hello"
+        4. Print the data type of each variable using type()
+"""
+
+# Create variables
+x = 5
+y = 3.14
+z = "Hello"
+
+# Print the data type of each variable
+print(type(x))
+print(type(y))
+print(type(z))
