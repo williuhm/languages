871fbde2bac60025540455d881ebce8979cb98ac
William Lister <billy09lister@gmail.com>
2026-04-16 09:45:41 -0500

    Finished W3Schools Comments Tutorial.

---
+++ Python/03-Comments/01-Comments.py
@@ -0,0 +1,21 @@
+"""
+    I've been writing comments this whole time, so again,
+    pretty easy concept. Not sure how there's a challenge
+    for this section but alright.
+"""
+
+#This is a comment
+print("Hello, World!") # This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+
+"""
+This is a comment
+written in
+more than just one line
+"""
---
+++ Python/03-Comments/02-Comments-Challenge.py
@@ -0,0 +1,25 @@
+"""
+    Challenge: Comments
+        Test your understanding of Python comments by completing a small coding challenge.
+
+    Instructions
+        Inside the editor, complete the following steps:
+        1. Add a single-line comment that says This is a comment
+        2. Comment out the line print("This should not run") so it does not execute
+        3. Add a multiline comment (using triple quotes) that says This is
+           a multiline
+           comment
+"""
+
+# Write a single-line comment
+# this is a comment
+
+# Comment out this line so it does not run:
+#print("This should not run")
+
+# Add a multiline comment
+"""
+This is
+a multiline
+comment
+"""
