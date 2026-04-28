f6e85fe9d75ff3da0ffee57c6453bae0f4f71440
William Lister <billy09lister@gmail.com>
2026-04-28 15:48:17 -0500

    Finished W3Schools Constants Tutorial.

---
+++ C/07-Constants/01-Constants.c
@@ -0,0 +1,15 @@
+/*
+    Constants are unchanged, so this will be an unchanging value.
+
+    Convention to make them all capitalized.
+*/
+
+#include <stdio.h>
+
+int main() {
+    const int minutesPerHour = 60;
+    const int monthsInYear = 12;
+
+    const int BIRTHYEAR = 1980;
+    return 0;
+}
