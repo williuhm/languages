dc5a3283f19a4c5c31c71c02ee806d4b1e5812f9
William Lister <billy09lister@gmail.com>
2026-04-28 12:46:25 -0500

    Finished W3Schools Comments Tutorial.

---
+++ C/03-Comments/01-Comments.c
@@ -0,0 +1,19 @@
+/*
+    I use a lot of comments (evidently) so this is good to
+    know. However, I think most of these languages are
+    similar, so I already started using comments a while
+    ago.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // This is a comment
+    printf("Hello World!"); // This is a comment
+    // printf("This will NOT run");
+
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    printf("Hello World!");
+    return 0;
+}
